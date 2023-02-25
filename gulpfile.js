5c11f0d6cb150b9fd9b007ac8c65d64f13107fb0
homodeus7 <plum.bus@yahoo.com>
2023-02-25 16:22:49 +0300

    added gallery markup

--- gulpfile.js
+++ gulpfile.js
@@ -140,7 +140,7 @@ const stylesBackend = () => {
 
 // scripts
 const scripts = () => {
-  return src([paths.srcMainJs, "node_modules/slick-carousel/slick/slick.js"])
+  return src([paths.srcMainJs, "node_modules/mixitup/dist/mixitup.js"])
     .pipe(
       plumber(
         notify.onError({
